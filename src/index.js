eb369586cd669146940976ec6e66446b501e84d8
pholohan <holohan.paul@gmail.com>
2020-07-23 21:36:52 +0100

    Upcoming Movies Feature Implemented

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'  // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
+import UpComingMoviesPage from "./pages/upcomingMoviesPage";
 import SiteHeader from './components/siteHeader'
 import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
@@ -17,12 +18,13 @@ const App = () => {
     <div className="jumbotron">
       <SiteHeader />      {/* New Header  */}
       <div className="container-fluid">
-        <MoviesContextProvider>     {/* NEW  */}
+      <MoviesContextProvider>     {/* NEW  */}
           <GenresContextProvider>    {/* NEW */}
             <Switch>
               <Route exact path="/reviews/form" component={AddMovieReviewPage} />
               <Route path="/reviews/:id" component={MovieReviewPage} />
               <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+              <Route exact path="/upcoming" component={UpComingMoviesPage} />
               <Route path="/movies/:id" component={MoviePage} />
               <Route path="/" component={HomePage} />
               <Redirect from="*" to="/" />
