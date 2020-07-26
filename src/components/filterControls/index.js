d253c2785d0bb9370d4c0230c23789747899b20b
pholohan <holohan.paul@gmail.com>
2020-07-26 20:04:46 +0100

    Commit for Filtering by Year Released

--- src/components/filterControls/index.js
+++ src/components/filterControls/index.js
@@ -15,10 +15,14 @@ const FilterControls = props => {
   const handleGenreChange = e => {
     handleChange(e, "genre", e.target.value);
   };
+  const handleDateChange = (e, type, value) => {
+    handleChange(e, "release_date", e.target.value);
+  };
+
 
   return (
     <div className="row bg-warning">
-      <div className="col-md-12">
+      <div className="col-md-5">
         <h4>
           <span>List Filtering:</span>
           <input
@@ -26,6 +30,10 @@ const FilterControls = props => {
             placeholder="Title Search"
             onChange={handleTextChange}
           />
+        </h4>
+      </div>
+      <div classname="col-md-3">
+        <h4>
           <span>Genre:</span>
           <select id="genre" onChange={handleGenreChange}>
             {context.genres.map(genre => {
@@ -36,6 +44,16 @@ const FilterControls = props => {
               );
             })}
           </select>
+        </h4>
+      </div>
+      <div classname="col-md-4">
+        <h4>
+          <span>Release Date:</span>
+          <input
+            type="text"
+            placeholder="Release Date"
+            onChange={handleDateChange}
+          />
         </h4>
       </div>
     </div>
